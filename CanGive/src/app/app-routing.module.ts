7e82ed084aa0ab6967e7e38067bc322b3e8207f1
joshd <joshd@LAPTOP-0NRV8MFN>
2021-06-05 15:06:27 +1200

    Created login component

--- CanGive/src/app/app-routing.module.ts
+++ CanGive/src/app/app-routing.module.ts
@@ -1,7 +1,20 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { LoginComponent } from './login/login.component';
+import { AboutComponent } from './views/about/about.component';
+import { HomeComponent } from './views/home/home.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: 'about', component: AboutComponent
+  },
+  {
+    path: '', component: HomeComponent
+  },
+  {
+    path: 'login', component: LoginComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
